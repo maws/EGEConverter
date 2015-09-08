5ec29b9cc475b967edbb5e87c671d49fb5142231
Max Wijk Stranius <maxws@europe.com>
2015-09-08 08:13:31 +0200

    Add indices support

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,32 @@
 
 using namespace fbxsdk_2015_1;
 
+struct Mesh
+{
+	Mesh() 
+	{ 
+		vertices = nullptr; 
+		indices = nullptr; 
+	}
+
+	~Mesh()
+	{
+		if (vertices) delete vertices;
+		if (indices) delete indices;
+	}
+
+	int numVertices;
+	float* vertices;
+	int numIndices;
+	int* indices;
+};
+
 int main(int argc, char *argv[])
 {
-	// The master vertices list used for exporting
-	float* vertexBuffer = nullptr; // Allocate when we know length
-	unsigned numVerts = 0;
+	// The export mesh we are going to fill with data and export
+	Mesh* exportMesh;
 
-	std::string fileName = argv[1];
+	std::string fileName = "cube.fbx";
 	printf("Importing %s \n", fileName.c_str());
 	
 	// Strip extension
@@ -78,26 +97,57 @@ int main(int argc, char *argv[])
 					{
 						FbxMesh* mesh = (FbxMesh*)node->GetNodeAttribute();
 						printf("Mesh name: %s \n", (char*)node->GetName());
-						
-						FbxVector4* controlPoints = mesh->GetControlPoints();
-						int triCount = mesh->GetPolygonCount();
-						numVerts = triCount * 3;
-						vertexBuffer = new float[numVerts * 3];
-						int vertexCounter = 0;
-						for (int j = 0; j < triCount; ++j)
+
+						// Create our export mesh and fill it
+						exportMesh = new Mesh();
+
+						// Fill vertices
+						exportMesh->numVertices = mesh->GetControlPointsCount();
+						exportMesh->vertices = new float[exportMesh->numVertices * 3];
+						for (int j = 0; j < exportMesh->numVertices; ++j)
 						{
-							for (unsigned k = 0; k < 3; ++k)
+							FbxVector4 point = mesh->GetControlPointAt(j);
+							float verts[3] = { point.mData[0], point.mData[1], point.mData[2] };
+							printf("Vertex %d:  %.1f %.1f %.1f \n", j, verts[0], verts[1], verts[2]);
+
+							int dstIndex = j * 3;
+							exportMesh->vertices[dstIndex + 0] = verts[0];
+							exportMesh->vertices[dstIndex + 1] = verts[1];
+							exportMesh->vertices[dstIndex + 2] = verts[2];
+						}
+
+						// Fill indices
+						exportMesh->numIndices = mesh->GetPolygonVertexCount();
+						exportMesh->indices = new int[exportMesh->numIndices];
+						for (int j = 0; j < exportMesh->numIndices; ++j)
+						{
+							int index = mesh->GetPolygonVertices()[j];
+							exportMesh->indices[j] = index;
+							FbxVector4 vertex = mesh->GetControlPoints()[index];
+							printf("Index %d has vertex   %.1f,  %.1f,  %.1f,   \n", index, vertex.mData[0], vertex.mData[1], vertex.mData[2]);
+						}
+
+
+						/* Test data
+						int controlPointCount = mesh->GetControlPointsCount();
+						FbxVector4 cpoints[8];
+						for (unsigned j = 0; j < controlPointCount; ++j)
+						{
+							cpoints[j] = mesh->GetControlPointAt(j);
+						}
+
+						int polygonCount = mesh->GetPolygonCount();
+						for (int poly = 0; poly < polygonCount; ++poly)
+						{
+							int polySize = mesh->GetPolygonSize(poly);
+							for (int polyVertex = 0; polyVertex < polySize; ++polyVertex)
 							{
-								int ctrlPointIndex = mesh->GetPolygonVertex(j, k);
-								FbxVector4 currentControlPoint = controlPoints[ctrlPointIndex];
-								float verts[3] = { static_cast<float>(currentControlPoint.mData[0]), static_cast<float>(currentControlPoint.mData[1]), static_cast<float>(currentControlPoint.mData[2]) };
-								printf("Vertex %d has coordinates %f %f %f \n", j, verts[0], verts[1], verts[2]);
-								
-								int dstIndex = (j * 3 * 3) + (k * 3);
-								memcpy(&vertexBuffer[dstIndex], verts, sizeof(verts));
-								++vertexCounter;
+								int index = mesh->GetPolygonVertex(poly, polyVertex);
+								FbxVector4 v = cpoints[index];
+								printf("Index %d has %.1f, %.1f, %.1f, \n", index, v.mData[0], v.mData[1], v.mData[2]);
 							}
 						}
+						*/
 					}
 					break;
 				}
@@ -105,36 +155,60 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	printf("Total vertices: %d \n", numVerts);
-	printf("Total values: %d \n", numVerts * 3);
-	printf("Exporting to e3m... \n");
+	printf("Mesh Stats: \n \n");
+	printf("Total vertices: %d \n", exportMesh->numVertices);
+	printf("Total indices: %d \n", exportMesh->numIndices);
+	printf("Size: %d bytes \n", ((sizeof(float) * 3) * exportMesh->numVertices) + (sizeof(int) * exportMesh->numIndices));
+	printf("Exporting to e3m format, please wait... \n");
 	fileNameRaw.append(".e3m");
 
 	{ // Write
 		FILE* file;
 		file = fopen(fileNameRaw.c_str(), "wb");
 		// Write the number of vertices
-		fwrite(&numVerts, sizeof(unsigned), 1, file);
+		fwrite(&exportMesh->numVertices, sizeof(int), 1, file);
 		// Write vertices
-		fwrite(vertexBuffer, sizeof(float), numVerts * 3, file);
+		fwrite(exportMesh->vertices, sizeof(float), exportMesh->numVertices * 3, file);
+		// Write number of indices
+		fwrite(&exportMesh->numIndices, sizeof(int), 1, file);
+		// Write indices
+		fwrite(exportMesh->indices, sizeof(int), exportMesh->numIndices, file);
 		fclose(file);
 	}
 
-	// This is how you read
-	/*
+	/* This is how you read
 	{ // Read
-		float* buffer = new float[numVerts * 3];
+		Mesh* importMesh = new Mesh();
+
 		FILE* file;
-		unsigned num = 0;
 		file = fopen(fileNameRaw.c_str(), "rb");
-		fread(&num, sizeof(unsigned), 1, file);
-		fread(buffer, sizeof(float), num * 3, file);
+
+		// Read vertices
+		fread(&importMesh->numVertices, sizeof(int), 1, file);
+		importMesh->vertices = new float[importMesh->numVertices * 3];
+		fread(importMesh->vertices, sizeof(float), importMesh->numVertices * 3, file);
+
+		// Read indices
+		fread(&importMesh->numIndices, sizeof(int), 1, file);
+		importMesh->indices = new int[importMesh->numIndices];
+		fread(importMesh->indices, sizeof(int), importMesh->numIndices, file);
+
 		fclose(file);
-		delete[] buffer;
+		
+		// Check data
+		float verts[8 * 3];
+		for (int i = 0; i < 24; ++i)
+			verts[i] = importMesh->vertices[i];
+		float idx[24];
+		for (int i = 0; i < 24; ++i)
+			idx[i] = importMesh->indices[i];
+
+		// Get rid of mesh
+		delete importMesh;
 	}*/
 
 	printf("...Finished: Cleaning up. \n");
-	delete vertexBuffer;
+	delete exportMesh;
 	manager->Destroy();
 	return 0;
 }
