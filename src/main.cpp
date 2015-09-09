5bcfe61aef2200237e6393fc734f3e8d6d991c94
Max Wijk Stranius <maxws@europe.com>
2015-09-09 20:39:43 +0200

    Add vertex color support

--- src/main.cpp
+++ src/main.cpp
@@ -23,36 +23,45 @@ struct Mesh
 
 	int numVertices;
 	float* vertices;
+	float* vertexColors;
 	int numIndices;
 	int* indices;
 };
 
 int main(int argc, char *argv[])
 {
-	// The export mesh we are going to fill with data and export
+	// The mesh instance being exported
 	Mesh* exportMesh;
+	exportMesh = new Mesh();
 
 	std::string fileName = "cube.fbx";
+	/*if (argv[1] == "-f")
+	{
+		fileName = argv[2];
+	}
+	else
+	{
+		printf("Usage: \n");
+		printf("-f <filename> \n");
+		return 0;
+	}*/
+	
 	printf("Importing %s \n", fileName.c_str());
 	
-	// Strip extension
-	int extIndex = fileName.find_last_of(".");
-	std::string fileNameRaw = fileName.substr(0, extIndex);
+	// Strip extension and store raw file name for future reference
+	std::string fileNameRaw = fileName.substr(0, fileName.find_last_of("."));
 
-	FbxManager* manager = FbxManager::Create();
-	FbxIOSettings* ios = FbxIOSettings::Create(manager, IOSROOT);
-	FbxScene* scene = FbxScene::Create(manager, "Scene");
+	// Initialize fbx sdk
+	FbxManager* fbxManager = FbxManager::Create();
+	FbxIOSettings* ios = FbxIOSettings::Create(fbxManager, IOSROOT);
+	FbxScene* scene = FbxScene::Create(fbxManager, "Scene");
 
 	// Get sdk version info
 	int sdkMajor, sdkMinor, sdkRev;
 	FbxManager::GetFileFormatVersion(sdkMajor, sdkMinor, sdkRev);
 
-	FbxImporter* importer = FbxImporter::Create(manager, "");
-	bool status = importer->Initialize(fileName.c_str(), -1, manager->GetIOSettings());
-
-	// Get file version info
-	int fileMajor, fileMinor, fileRev;
-	importer->GetFileVersion(fileMajor, fileMinor, fileRev);
+	FbxImporter* importer = FbxImporter::Create(fbxManager, "");
+	bool status = importer->Initialize(fileName.c_str(), -1, fbxManager->GetIOSettings());
 
 	if (!status)
 	{
@@ -60,7 +69,12 @@ int main(int argc, char *argv[])
 		FbxString error = importer->GetStatus().GetErrorString();
 		printf("Failed to initialize fbx importer. \n");
 		printf("Error: %s \n", error.Buffer());
+		return 0;
 	}
+
+	// Get file version info
+	int fileMajor, fileMinor, fileRev;
+	importer->GetFileVersion(fileMajor, fileMinor, fileRev);
 
 	printf("FBX SDK version: %d.%d.%d \n", sdkMajor, sdkMinor, sdkRev);
 	if (importer->IsFBX())
@@ -68,12 +82,23 @@ int main(int argc, char *argv[])
 		printf("FBX file format version: %d.%d.%d \n", fileMajor, fileMinor, fileRev);
 
 	}
+	else
+	{
+		printf("File format is not supported. \n");
+		return 0;
+	}
 
 	// Import scene
 	status = importer->Import(scene);
 
+	// Triangulate scene
+	// NOTE: We assume that the scene vertices are not already triangulated
+	FbxGeometryConverter* geometryConverter;
+	geometryConverter = new FbxGeometryConverter(fbxManager);
+	geometryConverter->Triangulate(scene, true);
+
+	// Traverse node
 	auto rootNode = scene->GetRootNode();
-	
 	if (rootNode)
 	{
 		for (int i = 0; i < rootNode->GetChildCount(); ++i)
@@ -98,56 +123,54 @@ int main(int argc, char *argv[])
 						FbxMesh* mesh = (FbxMesh*)node->GetNodeAttribute();
 						printf("Mesh name: %s \n", (char*)node->GetName());
 
-						// Create our export mesh and fill it
-						exportMesh = new Mesh();
-
-						// Fill vertices
-						exportMesh->numVertices = mesh->GetControlPointsCount();
+						// Grab vertices
+						int triCount = mesh->GetPolygonCount();
+						int currentVert = 0;
+						exportMesh->numVertices = triCount * 3;
 						exportMesh->vertices = new float[exportMesh->numVertices * 3];
-						for (int j = 0; j < exportMesh->numVertices; ++j)
+						for (int tri = 0; tri < triCount; ++tri)
 						{
-							FbxVector4 point = mesh->GetControlPointAt(j);
-							float verts[3] = { point.mData[0], point.mData[1], point.mData[2] };
-							printf("Vertex %d:  %.1f %.1f %.1f \n", j, verts[0], verts[1], verts[2]);
-
-							int dstIndex = j * 3;
-							exportMesh->vertices[dstIndex + 0] = verts[0];
-							exportMesh->vertices[dstIndex + 1] = verts[1];
-							exportMesh->vertices[dstIndex + 2] = verts[2];
-						}
-
-						// Fill indices
-						exportMesh->numIndices = mesh->GetPolygonVertexCount();
-						exportMesh->indices = new int[exportMesh->numIndices];
-						for (int j = 0; j < exportMesh->numIndices; ++j)
-						{
-							int index = mesh->GetPolygonVertices()[j];
-							exportMesh->indices[j] = index;
-							FbxVector4 vertex = mesh->GetControlPoints()[index];
-							printf("Index %d has vertex   %.1f,  %.1f,  %.1f,   \n", index, vertex.mData[0], vertex.mData[1], vertex.mData[2]);
-						}
-
-
-						/* Test data
-						int controlPointCount = mesh->GetControlPointsCount();
-						FbxVector4 cpoints[8];
-						for (unsigned j = 0; j < controlPointCount; ++j)
-						{
-							cpoints[j] = mesh->GetControlPointAt(j);
-						}
-
-						int polygonCount = mesh->GetPolygonCount();
-						for (int poly = 0; poly < polygonCount; ++poly)
-						{
-							int polySize = mesh->GetPolygonSize(poly);
-							for (int polyVertex = 0; polyVertex < polySize; ++polyVertex)
+							for (int vertex = 0; vertex < 3; ++vertex)
 							{
-								int index = mesh->GetPolygonVertex(poly, polyVertex);
-								FbxVector4 v = cpoints[index];
-								printf("Index %d has %.1f, %.1f, %.1f, \n", index, v.mData[0], v.mData[1], v.mData[2]);
+								int index = mesh->GetPolygonVertex(tri, vertex);
+								FbxVector4 v = mesh->GetControlPoints()[index];
+								auto c = mesh->GetElementVertexColor(index);
+								
+								printf("Vertex %d: %.1f, %.1f, %.1f, \n", currentVert, v.mData[0], v.mData[1], v.mData[2]);
+								
+								int dstIndex = currentVert * 3;
+								exportMesh->vertices[dstIndex + 0] = static_cast<float>(v.mData[0]);
+								exportMesh->vertices[dstIndex + 1] = static_cast<float>(v.mData[1]);
+								exportMesh->vertices[dstIndex + 2] = static_cast<float>(v.mData[2]);
+
+								++currentVert;
 							}
 						}
-						*/
+
+						// Note: Mesh indices was supported in an older format
+						// Add some dumb values for now
+						exportMesh->numIndices = 1;
+						exportMesh->indices = new int[1];
+						exportMesh->indices[0] = -1;
+
+						// Grab vertex colors
+						// TODO: Add vertex color support in e3m format
+						// TODO: Add support for multiple color layers
+						
+						// Example code
+						exportMesh->vertexColors = new float[exportMesh->numVertices * 4];
+						for (int i = 0; i < exportMesh->numVertices; ++i)
+						{
+							int colorIndex = mesh->GetLayer(0)->GetVertexColors()->GetIndexArray()[i];
+							FbxColor color = mesh->GetLayer(0)->GetVertexColors()->GetDirectArray()[colorIndex];
+
+							int dstIndex = i * 4;
+							exportMesh->vertexColors[dstIndex + 0] = color.mRed;
+							exportMesh->vertexColors[dstIndex + 1] = color.mGreen;
+							exportMesh->vertexColors[dstIndex + 2] = color.mBlue;
+							exportMesh->vertexColors[dstIndex + 3] = color.mAlpha;
+						}
+						
 					}
 					break;
 				}
@@ -155,20 +178,27 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	// Export mesh
+	unsigned sizeOfVertices = sizeof(float) * 3 * exportMesh->numVertices;
+	unsigned sizeOfIndices = sizeof(int) * exportMesh->numIndices;
+
 	printf("Mesh Stats: \n \n");
 	printf("Total vertices: %d \n", exportMesh->numVertices);
 	printf("Total indices: %d \n", exportMesh->numIndices);
-	printf("Size: %d bytes \n", ((sizeof(float) * 3) * exportMesh->numVertices) + (sizeof(int) * exportMesh->numIndices));
+	printf("Size: %d bytes \n", sizeOfVertices + sizeOfIndices + sizeof(exportMesh->numIndices) + sizeof(exportMesh->numVertices));
 	printf("Exporting to e3m format, please wait... \n");
-	fileNameRaw.append(".e3m");
+	
+	std::string exportFileName = fileNameRaw.append(".e3m");
 
 	{ // Write
 		FILE* file;
-		file = fopen(fileNameRaw.c_str(), "wb");
+		file = fopen(exportFileName.c_str(), "wb");
 		// Write the number of vertices
 		fwrite(&exportMesh->numVertices, sizeof(int), 1, file);
 		// Write vertices
 		fwrite(exportMesh->vertices, sizeof(float), exportMesh->numVertices * 3, file);
+		// Write vertex colors
+		fwrite(exportMesh->vertexColors, sizeof(float), exportMesh->numVertices * 4, file);
 		// Write number of indices
 		fwrite(&exportMesh->numIndices, sizeof(int), 1, file);
 		// Write indices
@@ -181,12 +211,16 @@ int main(int argc, char *argv[])
 		Mesh* importMesh = new Mesh();
 
 		FILE* file;
-		file = fopen(fileNameRaw.c_str(), "rb");
+		file = fopen(exportFileName.c_str(), "rb");
 
 		// Read vertices
 		fread(&importMesh->numVertices, sizeof(int), 1, file);
 		importMesh->vertices = new float[importMesh->numVertices * 3];
 		fread(importMesh->vertices, sizeof(float), importMesh->numVertices * 3, file);
+
+		// Read vertex colors
+		importMesh->vertexColors = new float[importMesh->numVertices * 4];
+		fread(importMesh->vertexColors, sizeof(float), importMesh->numVertices * 4, file);
 
 		// Read indices
 		fread(&importMesh->numIndices, sizeof(int), 1, file);
@@ -194,21 +228,13 @@ int main(int argc, char *argv[])
 		fread(importMesh->indices, sizeof(int), importMesh->numIndices, file);
 
 		fclose(file);
-		
-		// Check data
-		float verts[8 * 3];
-		for (int i = 0; i < 24; ++i)
-			verts[i] = importMesh->vertices[i];
-		float idx[24];
-		for (int i = 0; i < 24; ++i)
-			idx[i] = importMesh->indices[i];
 
 		// Get rid of mesh
 		delete importMesh;
 	}*/
-
-	printf("...Finished: Cleaning up. \n");
+	
+	printf("Finished: Cleaning up. \n");
 	delete exportMesh;
-	manager->Destroy();
+	fbxManager->Destroy();
 	return 0;
 }
